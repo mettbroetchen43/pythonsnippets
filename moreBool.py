4f2365e145ba55be4561d33354420337e442956c
Sascha Mester <mettbroetchen43@gmail.com>
2023-07-07 23:29:06 +0200

    Improvement on readability of the code

--- moreBool.py
+++ moreBool.py
@@ -3,23 +3,16 @@ def xor(expr1, expr2):
     The exclusive-or logic. One expression (and only one expression!) needs to be true.
     If both are true, the function returns False.
     '''
-    if (expr1 and not expr2) or (expr2 and not expr1):
-        return True
-    return False
+    return (expr1 and not expr2) or (expr2 and not expr1)
 
 def nor(expr1, expr2):
     '''
     Compares two expressions and returns whether both are False. Neither expr1 nor expr2 may be true to return True. 
     '''
-    if (not expr1) and (not expr2):
-        return True
-    return False
+    return (not expr1) and (not expr2)
 
 def inv(expr):
     '''
     Inverts the input signal. A true expression returns false vice-versa.
     '''
-    if (not expr):
-        return True
-    return False
-    
+    return (not expr)    
