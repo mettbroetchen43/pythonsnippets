0347b2cc77ca23e963f058cc85251153a50cb4aa
Sascha Mester <mettbroetchen43@gmail.com>
2023-07-10 11:01:21 +0200

    Introduces check whether given expressions are Bool

--- moreBool.py
+++ moreBool.py
@@ -22,24 +22,35 @@
 
 
 
+def isBool(expr):
+    '''
+    Checks whether Expression is of type Bool, which is fundamentally 
+    important here.
+    '''
+    return isinstance(expr, bool)
+
 def xor(expr1, expr2):
     '''
     The exclusive-or logic. One expression (and only one expression!) needs to be true.
     If both are true or both are false, the function returns False.
     '''
-    return (expr1 and not expr2) or (expr2 and not expr1)
+
+    if isBool(expr1) and isBool(expr2):
+        return (expr1 and not expr2) or (expr2 and not expr1)
 
 def inv(expr):
     '''
     Inverts the input signal. A true expression returns false vice-versa.
     '''
-    return (not expr)    
+    if isBool(expr):
+        return (not expr)    
 
 def nor(expr1, expr2):
     '''
     Compares two expressions and returns whether both are False. Neither expr1 nor expr2 may be true to return True. 
     '''
-    return inv(expr1) and inv(expr2)
+    if isBool(expr1) and isBool(expr2):
+        return inv(expr1) and inv(expr2)
 
 def xnor(expr1, expr2):
     '''
@@ -48,14 +59,16 @@ def xnor(expr1, expr2):
     Also returns True if expr1 and expr2 are False.
     Returns False if expr1 is True and expr2 is False vice-versa.
     '''
-    return (expr1 and expr2) or (inv(expr1) and inv(expr2))
+    if isBool(expr1) and isBool(expr2):
+        return (expr1 and expr2) or (inv(expr1) and inv(expr2))
 
 def nand(expr1, expr2):
     '''
     Compares two expressions and returns whether at least one of them is false.
     Returns True if at least one of two expressions is false.
     '''
-    return inv(expr1) or inv(expr2)
+    if isBool(expr1) and isBool(expr2):
+        return inv(expr1) or inv(expr2)
 
 def xnand(expr1, expr2):
     '''
@@ -63,5 +76,6 @@ def xnand(expr1, expr2):
     Returns True if one of two expressions is false. If both expressions
     are true or both expressions are false, this function returns False.
     '''
-    return xor(inv(expr1), inv(expr2))
+    if isBool(expr1) and isBool(expr2):
+        return xor(inv(expr1), inv(expr2))
 
