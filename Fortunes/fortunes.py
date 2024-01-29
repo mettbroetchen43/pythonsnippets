a8a7f0624d1000d894707bd6be11ae2822fc385a
Sascha Mester <mettbroetchen43@gmail.com>
2024-01-29 15:53:39 +0100

    Translated comment in 'fortunes.py' into the english language.

--- Fortunes/fortunes.py
+++ Fortunes/fortunes.py
@@ -2,7 +2,7 @@ import random
 
 def get_random_fortune():
     with open('fortunes.txt', 'r') as f:
-        fortunes = f.read().split('%')[:-1]  # Schneide das letzte Element (leerer String) ab
+        fortunes = f.read().split('%')[:-1]  # Cut off last element which might be an empty string.
 
     return random.choice(fortunes).strip()
 
