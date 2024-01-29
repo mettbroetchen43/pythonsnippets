119a026c420522c587bee446493bb8874725bfe8
Sascha Mester <mettbroetchen43@gmail.com>
2024-01-29 15:51:13 +0100

    Encapsulated 'fortunes.py' and 'main.py' for modularization

--- Fortunes/fortunes.py
+++ Fortunes/fortunes.py
@@ -2,10 +2,10 @@ import random
 
 def get_random_fortune():
     with open('fortunes.txt', 'r') as f:
-        fortunes = f.read().split('%')[:-1]  # Cut off the last element which might be an empty string.
+        fortunes = f.read().split('%')[:-1]  # Schneide das letzte Element (leerer String) ab
 
     return random.choice(fortunes).strip()
 
 if __name__ == "__main__":
-    random_fortune = get_random_fortune()
-    print(random_fortune)
+    print("This file is not meant to be run directly.")
+    print("Please run 'main.py' instead.")
---
+++ Fortunes/main.py
@@ -0,0 +1,5 @@
+import fortunes 
+
+if __name__ == '__main__':
+    print (fortunes.get_random_fortune())
+
